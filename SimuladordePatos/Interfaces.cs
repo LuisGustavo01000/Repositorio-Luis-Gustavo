b34f3256d6df8d63ecb0f968eac045319c7b1e28
Luis Gustavo <luisgustavodonascimentomarinho@gmail.com>
2025-05-01 14:38:12 -0300

    Primeira parte Simulador de patos

---
+++ SimuladordePatos/Interfaces.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimuladordePatos
+{
+    public interface IFlyable
+    {
+        void fly();
+    }
+
+    public interface IQuackable
+    {
+        void quack();
+    }
+
+    public interface ISwinable
+    {
+        void swin();
+    }
+}
---
+++ SimuladordePatos/Program.cs
@@ -0,0 +1,42 @@
+﻿using SimuladordePatos;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("---- Simulador de Patos -------\n");
+
+        MallardDuck duck = new MallardDuck("Pato Normal");
+        duck.display();
+        duck.fly();
+        duck.quack();
+        duck.swin();
+
+        MuteDuck duckmute = new MuteDuck("Pato Mudo");
+        duckmute.display();
+        duckmute.fly();
+        duckmute.swin();
+
+        RedHeadDuck duckred = new RedHeadDuck("Pato Vermelho");
+        duckred .display(); 
+        duckred.fly();
+        duckred.quack();
+        duckred.swin();
+
+        RuberDuck duckru = new RuberDuck("Pato de Borracha");
+        duckru .display(); 
+        duckru.quack();
+
+        RocketDuck duckrocket = new RocketDuck("Pato Foguete");
+        duckrocket .display();
+        duckrocket.fly();
+
+        SuperDuck ducks = new SuperDuck("Super Pato");
+        ducks .display();
+        ducks .fly();
+        ducks.quack();
+        ducks.swin();
+
+
+    }
+}
